cbb55293bc0c18aa294503244db9b8ce3351e909
Eldad <eldadf456@gmail.com>
2023-10-18 11:07:34 +0300

    first commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// import {Company} from './User';
+
+// const company = new Company();
+// console.log(company);
+
+
+ new google.maps.Map(document.getElementById('map'), {
+   zoom: 1,
+  center: {
+    lat: 0,
+   lng: 0
+  }
+})
